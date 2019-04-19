f5e7bf70a90f9388790d3d88e025fa40c18c923c
rooocket <info@rooocket>
2019-04-19 16:10:56 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 include("Tl.php");
-
-$telegramAPI = new Tl();
-
-//Получаем сообщения
-$updates = $telegramAPI->getUpdates();
-
-echo '<pre>';
-var_dump($updates);
-echo '</pre>';
+//
+//$telegramAPI = new Tl();
+//
+////Получаем сообщения
+//$updates = $telegramAPI->getUpdates();
+//
+//echo '<pre>';
+//var_dump($updates);
+//echo '</pre>';
 /*
 //Проходим по сообщения
 foreach($updates as $update) {
     $telegramAPI->sendMessage($update->message->chat->id, 'Hello');
 }*/
+
+$result = fopen("https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/sendMessage?chat_id=293854654&parse_mode=html&text='hello'","r");
