5c6278f4f2f02c07dc03a2264446f300b4c9f948
rooocket <info@rooocket>
2019-04-19 16:01:36 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ $telegramAPI = new Tl();
 //Получаем сообщения
 $updates = $telegramAPI->getUpdates();
 
+echo '<pre>';
+var_dump($updates);
+echo '</pre>';
+
 //Проходим по сообщения
 foreach($updates as $update) {
     $telegramAPI->sendMessage($update->message->chat->id, 'Hello');
