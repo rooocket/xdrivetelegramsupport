390dff5926893da507701ceb5d0ec1a8135e39c9
rooocket <info@rooocket>
2019-04-19 16:47:55 +0300

    Тестрование соединения

--- Tl.php
+++ Tl.php
@@ -7,8 +7,8 @@ class Tl
     private $url     = 'https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U';
 
     public function getUpdates() {
-        $response = file_get_contents($this->url . '/getUpdates');
-        return json_decode($response);
+        $response = json_decode(file_get_contents($this->url . '/getUpdates'));
+        return $response->result;
     }
 
     public function sendMessage($chat_id, $text) {
