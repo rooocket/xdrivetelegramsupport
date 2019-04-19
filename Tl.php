db9807b2de3a21726fcb9566f7e95764c39e83a4
rooocket <info@rooocket>
2019-04-19 16:44:32 +0300

    Тестрование соединения

--- Tl.php
+++ Tl.php
@@ -4,39 +4,23 @@
 class Tl
 {
 
-    private $token = '762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U'; //url telegram
-
-    public function query($method, $params = []) {
-        $url        = 'https://api.telegram.org/bot';
-        $url        .= $this->token . '/';
-        $url        .= $method ;
-        if(!empty($params)) {
-            $k = 0;
-            foreach($params as $key=>$value) {
-                $url .= ($k == 0 ? '/?' : '&') . $key . '=' . $value;
-                $k++;
-            }
-        }
-
-        $result = fopen($url,"r");
-        return $result;
-    }
+    private $url     = 'https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U';
 
     public function getUpdates() {
-        $response = $this->query('getUpdates');
+        $response = $this->url . '/getUpdates';
         return json_decode($response);
     }
 
     public function sendMessage($chat_id, $text) {
         //send
-        $response = $this->query('sendMessage',
-            [
-                'text' => $text,
-                'chat_id' => $chat_id,
-                'parse_mode' => 'html'
-            ]
-        );
-        return $response;
+//        $response = $this->query('sendMessage',
+//            [
+//                'text' => $text,
+//                'chat_id' => $chat_id,
+//                'parse_mode' => 'html'
+//            ]
+//        );
+//        return $response;
     }
 
 }
