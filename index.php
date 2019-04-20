94028da42dbb6b4ed9a13ef7a51705d96bea4381
rooocket <info@rooocket>
2019-04-20 13:47:00 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -1,106 +1,32 @@
 <?php
-ini_set('display_errors',1);
-error_reporting(E_ALL);
-//include("Tl.php");
-//$t = new Tl();
-//$upd = $t->getUpdates();
-//echo '<pre>';
-//var_dump($upd);
-//echo '</pre>';
-$response = fopen('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/setwebhook?url=https://xdrivetelegramsupport.herokuapp.com/index.php', 'r');
 
-//
-//function sendMessage($chat_id, $message, $first_name)
-//{
-//    $message = 'Здравствуй, ' . $first_name . ', требуется регистрация, нажмите на кнопку Отправить сообщение' . $message;
-//    $keyboard = array(
-//        "keyboard" => array(array(
-//            array(
-//                "text" => "Отправить номер телефона",
-//                "request_contact" => true
-//
-//            )
-//        )),
-//        "one_time_keyboard" => true,
-//        "resize_keyboard" => true
-//    );
-//
-//    $keyboard = '&reply_markup=' . json_encode($keyboard);
-//    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . $keyboard);
-//
-//    $keyboard = '';
-//    //Проверяем, была ли регистрация?
-//    if(file_exists('https://xdrive.faberlic.com/files/telegram_reg/' . $chat_id . '.txt')) {
-//
-//    } else {
-//        //Просим прислать номер телефона
-//        $keyboard = '';
-//        $mes = preg_replace('![^0-9]+!', '', $message);
-//        $mes = strlen($mes);
-//        if($mes > 10) {
-//            $message = 'Вы ввели номер телефона: ' . $message . ' (' . $mes . ')';
-//        } elseif($mes == 4){
-//            $message = 'Вы код подтверждения: ' . $message . ' (' . $mes . ')';
-//        } else {
-//            $message = 'Здравствуй, ' . $first_name . ', требуется регистрация, нажмите на кнопку Отправить сообщение' . $message;
-//            $keyboard = array(
-//                "keyboard" => array(array(
-//                    array(
-//                        "text" => "Отправить номер телефона",
-//                        "request_contact" => true
-//
-//                    )
-//                )),
-//                "one_time_keyboard" => true,
-//                "resize_keyboard" => true
-//            );
-//
-//            $keyboard = '&reply_markup=' . json_encode($keyboard);
-//
-//        }
-//
-//        file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . $keyboard);
-//    }
-//
-//
-//    /*
-//        if($message == 'hi' || $message == 'привет') {
-//            $message = $first_name . ', я приветствую тебя. Для перехода в меню введи /start или /menu';
-//        }
-//        elseif($message == '999-999'){
-//             $url = 'https://xdrive.faberlic.com/api/api_telegram.php';
-//            $context = stream_context_create([
-//                'http' => [
-//                    'method' => 'POST',
-//                    'content' => http_build_query([
-//
-//                        'key' 			=> '54670a1ad18ce5842eea01499c12ed5a',	//ключ
-//                        'action' 		=> 'CheckRegistration',	                //метод
-//                        'login' 		=> $message,							//телефон
-//                        'chat_id' 		=> $chat_id,							//телефон
-//                    ])
-//                ]
-//            ]);
-//
-//            file_get_contents($url, false, $context);
-//        }
-//
-//        else {
-//            $message = $first_name . ', Ты написал: ' . $message;
-//        }
-//    */
-//
-//}
-//
-//$access_token = '762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U';
-//$api = 'https://api.telegram.org/bot' . $access_token;
-//
-//
-//$output = json_decode(file_get_contents('php://input'), TRUE);
-//$chat_id = $output['message']['chat']['id'];
-//$first_name = $output['message']['chat']['first_name'];
-//$message = $output['message']['text'];
-//$reply_markup = $output['message']['reply_markup']->keyboard;
-//
-////$preload_text = $first_name . ', я получила ваше сообщение!' . $message;
-//sendMessage($chat_id, $message, $first_name);
+function sendMessage($chat_id, $message)
+{
+    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+}
+
+$access_token = '762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U';
+$api = 'https://api.telegram.org/bot' . $access_token;
+
+
+$output         = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id        = $output['message']['chat']['id'];
+$contact        = isset($output['message']['contact']['phone_number']) ? $output['message']['contact']['phone_number'] : '';
+$first_name     = $output['message']['chat']['first_name'];
+$message        = $output['message']['text'];
+$message_t      = '';
+
+$number = preg_replace('![^0-9]+!', '', $message);
+if(!empty($contact)) {
+    $message_t = 'На Ваш номер телефона +' . $contact . ' отправлено SMS-сообщение с кодом доступа. Такой же номер телефона у вас должен быть указан в проекте xDrive/';
+}
+
+elseif(strlen($number) == 4) {
+    $message_t = 'Началась проверка введенного кода доступа';
+}
+
+else {
+    $message_t = 'Привет, ' . $first_name . '. Меня зовут xDriveSupportBot. Чтобы мной воспользоваться, необходимо подтвердить свой номер телефона. Такой же номер телефона должен быть указан в вашем личном кабинете xDrive.';
+}
+
+sendMessage($chat_id, $message_t);
