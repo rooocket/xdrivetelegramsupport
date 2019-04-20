ed409e0fbf45895b783bb92e3625d3814903ce78
rooocket <info@rooocket>
2019-04-20 16:05:43 +0300

    Тестрование соединения

--- query.php
+++ query.php
@@ -5,7 +5,7 @@ class Query
 {
 
     function xDriveQuery($array) {
-        $url = 'https://xdrive.faberlic.com/api/api_sms.php';
+        $url = 'https://xdrive.faberlic.com/api/api_telegram.php';
         $array['key'] = '54670a1ad18ce5842eea01499c12ed5a';
         $context = stream_context_create([
             'http' => [
