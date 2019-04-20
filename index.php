72b79d39cf188acbe301452204808bdf19fba365
rooocket <info@rooocket>
2019-04-20 12:16:14 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -5,64 +5,57 @@ include("Tl.php");
 
 function sendMessage($chat_id, $message, $first_name)
 {
-/*
-    if($message == 'hi' || $message == 'привет') {
-        $message = $first_name . ', я приветствую тебя. Для перехода в меню введи /start или /menu';
-    }
-    elseif($message == '999-999'){
-         $url = 'https://xdrive.faberlic.com/api/api_telegram.php';
-        $context = stream_context_create([
-            'http' => [
-                'method' => 'POST',
-                'content' => http_build_query([
-
-                    'key' 			=> '54670a1ad18ce5842eea01499c12ed5a',	//ключ
-                    'action' 		=> 'CheckRegistration',	                //метод
-                    'login' 		=> $message,							//телефон
-                    'chat_id' 		=> $chat_id,							//телефон
-                ])
-            ]
-        ]);
-
-        file_get_contents($url, false, $context);
-    }
-
-    else {
-        $message = $first_name . ', Ты написал: ' . $message;
-    }
-*/
-    $message = 'Вы ввели символов: ' . mb_strlen($message, 'utf-8');
-    //$mes = preg_replace('![^0-9]+!', '', $message);
-    $keyboard = '';
     /*
-    if(mb_strlen($mes) > 10) {
-        //Отправляем смс
-        $message = 'Вам на телефон +' . $message . ' отправлено смс с кодом подтверждения';
-    } elseif(mb_strlen($mes) == 4) {
-        //вводим код
-        $message = 'Введите код из смс';
+        if($message == 'hi' || $message == 'привет') {
+            $message = $first_name . ', я приветствую тебя. Для перехода в меню введи /start или /menu';
+        }
+        elseif($message == '999-999'){
+             $url = 'https://xdrive.faberlic.com/api/api_telegram.php';
+            $context = stream_context_create([
+                'http' => [
+                    'method' => 'POST',
+                    'content' => http_build_query([
 
-        //если вводим верный пароль. то записываем chat_id и номер телефона
-    } else {
-        $message = $first_name. ', Требуется регистрация';
-        //выводим сообщение о регистрации
-        $keyboard = array(
-            "keyboard" => array(
-                array(
-                    array(
-                        "text" => "Отправить номер телефона",
-                        "request_contact" => true
-                        )
-                    )
+                        'key' 			=> '54670a1ad18ce5842eea01499c12ed5a',	//ключ
+                        'action' 		=> 'CheckRegistration',	                //метод
+                        'login' 		=> $message,							//телефон
+                        'chat_id' 		=> $chat_id,							//телефон
+                    ])
+                ]
+            ]);
+
+            file_get_contents($url, false, $context);
+        }
+
+        else {
+            $message = $first_name . ', Ты написал: ' . $message;
+        }
+    */
+
+    $message = $first_name . ', требуется регистрация в боте';
+    $keyboard = array(
+        "keyboard" => array(array(array(
+            "text" => "/button"
+
+        ),
+            array(
+                "text" => "contact",
+                "request_contact" => true // Данный запрос необязательный telegram button для запроса номера телефона
+
             ),
-            "one_time_keyboard" => true,
-            "resize_keyboard" => true
-        );
-    }
+            array(
+                "text" => "location",
+                "request_location" => true // Данный запрос необязательный telegram button для запроса локации пользователя
 
-*/
+            )
 
-    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . ($keyboard ? '&reply_markup=' . json_encode($keyboard) : ''));
+        )),
+        "one_time_keyboard" => true, // можно заменить на FALSE,клавиатура скроется после нажатия кнопки автоматически при True
+        "resize_keyboard" => true // можно заменить на FALSE, клавиатура будет использовать компактный размер автоматически при True
+    );
+
+
+    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&reply_markup');
 }
 
 $access_token = '762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U';
