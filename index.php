9187a2af9ea3969fc3d29b683a73bad40a94e15a
rooocket <info@rooocket>
2019-04-20 10:17:53 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -3,36 +3,16 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 include("Tl.php");
 
+function sendMessage($chat_id, $message, $first_name)
+{
 
-
-
-/*
-$telegramAPI = new Tl();
-
-while(true) {
-
-    sleep(10);
-
-
-//Получаем сообщения
-    $updates = $telegramAPI->getUpdates();
-
-    echo '<pre>';
-    var_dump($updates);
-    echo '</pre>';
-//Проходим по сообщения
-    foreach($updates as $update) {
-        $text = 'Ты мне написал: ' . $update->message->text;
-        $telegramAPI->sendMessage($update->message->chat->id, $text);
+    if($message == 'hi' || $message == 'привет') {
+        $message = $first_name . ', я приветствую тебя. Для перехода в меню введи /start или /menu';
+    } else {
+        $message = $first_name . ', Ты написал: ' . $message;
     }
 
 
-}
-
-*/
-
-function sendMessage($chat_id, $message)
-{
     file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
 }
 
@@ -45,14 +25,5 @@ $chat_id = $output['message']['chat']['id'];
 $first_name = $output['message']['chat']['first_name'];
 $message = $output['message']['text'];
 
-$preload_text = $first_name . ', я получила ваше сообщение!';
-sendMessage($chat_id, $preload_text);
-/*
-$res = file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/setwebhook?url=https://xdrivetelegramsupport.herokuapp.com/index.php');
-
-echo '<pre>';
-var_dump($res);
-echo '</pre>';
-*/
-//$telegramAPI->sendMessage(293854654, 'Hello');
-//$result = fopen("https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/sendMessage?chat_id=293854654&parse_mode=html&text='hello'","r");
+//$preload_text = $first_name . ', я получила ваше сообщение!' . $message;
+sendMessage($chat_id, $message, $first_name);
