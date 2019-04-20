178a02412cd63acfbe1b8787b9331e9c67e3f505
rooocket <info@rooocket>
2019-04-20 10:07:13 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 include("Tl.php");
+
+
+
+
 /*
 $telegramAPI = new Tl();
 
@@ -26,7 +30,11 @@ while(true) {
 }
 
 */
-file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/setwebhook?url=https://xdrivetelegramsupport.herokuapp.com/index.php');
+$res = file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/setwebhook?url=https://xdrivetelegramsupport.herokuapp.com/index.php');
+
+echo '<pre>';
+var_dump($res);
+echo '</pre>';
 
 //$telegramAPI->sendMessage(293854654, 'Hello');
 //$result = fopen("https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/sendMessage?chat_id=293854654&parse_mode=html&text='hello'","r");
