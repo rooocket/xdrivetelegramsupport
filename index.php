30a677a0ede0a956f0b54933182b5f13cda75ca6
rooocket <info@rooocket>
2019-04-21 10:21:58 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -8,5 +8,9 @@ $query = new Query();
 //293854654 - Саша Жаров
 $admin_array = array(293854654);
 
-file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/deleteWebhook');
-echo 'deletted';
+$updatest = file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/getUpdates');
+$updatest = json_decode($updatest);
+
+echo '<pre>';
+var_dump($updatest);
+echo '</pre>';
