872d11d5744124a6e37a8b847071cdd44715d51e
rooocket <info@rooocket>
2019-04-21 10:37:04 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ $admin_array = array(293854654);
 
 $updatest = json_decode(file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/getUpdates'), TRUE);
 $output = $updatest;
-$output = $output->result[0];
 echo '<pre>';
 var_dump($output);
 echo '</pre>';
 
-$chat_id        = $output->message->chat->id;
-$contact        = isset($output->message->contact->phone_number) ?$output->message->contact->phone_number : '';
-$first_name     = $output->message->chat->first_name;
-$message        = $output->message->text;
+$output         = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id        = $output['message']['chat']['id'];
+$contact        = isset($output['message']['contact']['phone_number']) ? $output['message']['contact']['phone_number'] : '';
+$first_name     = $output['message']['chat']['first_name'];
+$message        = $output['message']['text'];
 $message_t      = '';
 $param          = '';
