2c1d9208e01801431e9499e4937ebc8c8f3e8735
rooocket <info@rooocket>
2019-04-22 00:57:30 +0300

    Тестрование соединения

--- send_admin.php
+++ send_admin.php
@@ -1,6 +1,8 @@
 <?php
 
-$chat_id = $_POST['chat_id'];
-$text = $_POST['text'];
+$chat_id = $_REQUEST['chat_id'];
+$text = $_REQUEST['text'];
 
 file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($text) );
+
+echo $chat_id . ' / ' . $text;
