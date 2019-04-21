01dabc37c0fe851416c8946231d35be407b473be
rooocket <info@rooocket>
2019-04-21 10:19:56 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -9,3 +9,4 @@ $query = new Query();
 $admin_array = array(293854654);
 
 file_get_contents('https://api.telegram.org/bot762331141:AAGztjW4kC40IHXY8yY3SrRjeVDtVeM0V0U/deleteWebhook');
+echo 'deletted';
