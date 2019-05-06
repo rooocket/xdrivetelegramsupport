b0c7494dfd2fcb18de60785febcfd8943a1d825e
rooocket <info@rooocket>
2019-05-06 16:53:14 +0300

    Тестрование соединения

--- index.php
+++ index.php
@@ -154,7 +154,7 @@ if(!empty(file_get_contents('https://xdrive.faberlic.com/files/telegram_reg/' . 
         if($q == 0) {
             $message_t = 'Я не могу предоставить Вам доступ.' .$error_text;
         } else {
-            $message_t = 'На Ваш номер телефона ' . $contact . ' отправлено SMS-сообщение с кодом доступа.';
+            $message_t = 'На Ваш номер телефона ' . $q . ' отправлено SMS-сообщение с кодом доступа.';
         }
 
     }
