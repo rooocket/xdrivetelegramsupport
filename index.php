c91e29c54a0b1849ead57dc66cee08ae2ab078cc
rooocket <info@rooocket>
2020-08-06 11:25:34 +0300

    Добавлено описание запроса в $error_text

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $message        = $output['message']['text'];
 $message_t      = '';
 $param          = isset($_REQUEST['param']) ? $_REQUEST['param'] : '';
 $parse_mode     = isset($_REQUEST['parse_mode']) ? $_REQUEST['parse_mode'] : '';
-$error_text     = ' Request_error: ' . json_encode($_REQUEST);
+$error_text     = ' Request_error: ' . json_encode($output);
 
 /*
  * Отправка личного сообщения
