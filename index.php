f0e3b36c1fa7a52c8c6fe020a530dd6add001f5e
rooocket <info@rooocket.ru>
2020-12-27 21:58:29 +0300

    Обновлена регистрация

--- index.php
+++ index.php
@@ -63,10 +63,211 @@ $error_text     = ' Request_error: ' . $message_t;
 
 //Проверяем, есьб ли аккаунт в телеграме
 $user_info      = $query->xDriveQuery(array('action'=>'check_user','chat_id'=>$chat_id));
+$user_info_row = json_decode($user_info);
 
+if(!empty($user_info_row) && $user_info_row->active == 1) {
+
+    if(empty($user_info_row->request)) {
+
+        $keyboard = array(
+            "keyboard" => array(
+                array(
+                    array(
+                        "text" => "Создать жалобу"
+                    ),
+                    array(
+                        "text" => "Статус заявки"
+                    ),
+                    array(
+                        "text" => "Статистика за день"
+                    )
+                )
+            ),
+            "one_time_keyboard" => false, // можно заменить на FALSE,клавиатура скроется после нажатия кнопки автоматически при True
+            "resize_keyboard" => false // можно заменить на FALSE, клавиатура будет использовать компактный размер автоматически при True
+        );
+        $param = '&reply_markup=' . json_encode($keyboard);
+
+        /******************************************************************************/
+        if($message == 'Создать жалобу') {
+            $message_t = $first_name . ", Введите 9-тизначный регистрационный номер заявки и текст сообщения, по которой у вас жалоба.\n\nПример, 71******* слишком молодая девушка";
+            $array = array(
+                'action' => 'request_update',
+                'type' => 'create_complaint',
+                'chat_id' => $chat_id
+            );
+            $q = $query->xDriveQuery($array);
+        }
+        /******************************************************************************/
+        elseif ($message == 'Статус заявки') {
+            $message_t = $first_name . ", введите числовой номер заявки или регистрационный номер консультанта. ";
+            $array = array(
+                'action' => 'request_update',
+                'type' => 'application_status',
+                'chat_id' => $chat_id
+            );
+            $q = $query->xDriveQuery($array);
+        }
+        /******************************************************************************/
+        elseif ($message == 'Статистика за день') {
+            $array = array(
+                'action' => 'stat',
+                'chat_id' => $chat_id
+            );
+            $q = $query->xDriveQuery($array);
+            $message_t = $q;
+        }
+        /******************************************************************************/
+        elseif($message == '/chat_id') {
+            $message_t = 'Ваш Chat_id: ' . $chat_id;
+        }
+        /******************************************************************************/
+        else {
+
+            $message_t = "Привет, " . $first_name . ".\n\nЕсли хотите пожаловаться на заявку - Нажмите на кнопку «Создать жалобу», если хотите проверить статус выполнения заявки - «Статус заявки» под клавиатурой.";
+        }
+        /******************************************************************************/
+    } else {
+
+
+        $keyboard = array(
+            "keyboard" => array(
+                array(
+                    array(
+                        "text" => "Создать жалобу"
+                    ),
+                    array(
+                        "text" => "Статус заявки"
+                    ),
+                    array(
+                        "text" => "Статистика за день"
+                    )
+                )
+            ),
+            "one_time_keyboard" => false, // можно заменить на FALSE,клавиатура скроется после нажатия кнопки автоматически при True
+            "resize_keyboard" => false // можно заменить на FALSE, клавиатура будет использовать компактный размер автоматически при True
+        );
+
+        $param = '&reply_markup=' . json_encode($keyboard);
+
+        //Если выполняется сложный запрос с вводом
+        $type = $user_info_row->request;
+        if($type == 'create_complaint') {
+            preg_match("/([0-9]*) (.*)/",$message,$m_arr);
+            if(!empty($m_arr[1]) && !empty($m_arr[2])) {
+
+                if(strlen($m_arr[1]) == 9) {
+                    $array = array(
+                        'action'    => 'add',
+                        'number'    => $m_arr[1],
+                        'text'      => trim($m_arr[2]),
+                        'chat_id'   => $chat_id
+                    );
+                    $q = $query->xDriveQuery($array);
+
+
+                    if($q == 0) {
+                        $message_t = 'Ошибка добавления заявки. - ' . $message;
+                    } elseif($q == 2) {
+                        $message_t = 'В xDrive нет консультанта с регистрационным номером ' . $m_arr[1];
+                    } else {
+                        $message_t = 'Вашей заявке присвоен №' . $q . '. Мы отправим вам ответ в ближайшее время.';
+                    }
+                } else {
+                    $message_t = "Ошибка!\n\nНеправильно введен регистрационный номер консультанта.\n\nПример, 71******* слишком молодая девушка";
+                }
+
+
+            } else {
+                $message_t = "Не правильно введен запрос.\n\nПример, 71******* слишком молодая девушка";
+            }
+        }
+        elseif($type == 'application_status') {
+            $id = preg_replace('![^0-9]+!', '', $message);
+            $array = array(
+                'action'    => 'find',
+                'id'        => $id,
+                'chat_id'   => $chat_id
+            );
+            $q = $query->xDriveQuery($array);
+
+            if($q == 0) {
+                $message_t = 'Заявка не найдена.' . $error_text;
+            } else {
+                $message_t = $q;
+            }
+
+            $message_t = $q;
+        }
+        else {
+            $message_t = 'Ошибка выполнения запроса. Тип запроса не найден. ' . $error_text;
+        }
+    }
+} else {
+    //Регистрация
+    $my_request = $user_info_row->request;
+    if(empty($my_request)) {
+        //Первый запрос, просим ввести пару логин пароль
+        $message_t = "Привет, " . $first_name . ".\n\nМеня зовут xDriveSupportBot. Чтобы мной воспользоваться, необходимо пройти регистрацию, чтобы привязать свой личный кабинет xdrive.faberlic.com к аккаунту Telegram.\n\nЗайдите свой профиль в личном кабинете https://xdrive.faberlic.com/?act=profile, скопируйте значение поля xCode и отправьте его мне.";
+        $array = array(
+            'action'    => 'reg_xDrive',
+            'chat_id'   => $chat_id
+        );
+        $q = $query->xDriveQuery($array);
+    } elseif ($my_request == '') {
+        //Вводи логина и пароля
+
+        $array = array(
+            'action'    => 'reg_xDrive',
+            'chat_id'   => $chat_id,
+            'login_password' => $message
+        );
+        $q = $query->xDriveQuery($array);
+        if($q == 1) {
+            //Успешная регистрация
+            $message_t = 'Вы успешно зарегистрированы в xDriveSupportBot';
+            $keyboard = array(
+                "keyboard" => array(
+                    array(
+                        array(
+                            "text" => "Создать жалобу"
+                        ),
+                        array(
+                            "text" => "Статус заявки"
+                        ),
+                        array(
+                            "text" => "Статистика за день"
+                        )
+                    )
+                ),
+                "one_time_keyboard" => false, // можно заменить на FALSE,клавиатура скроется после нажатия кнопки автоматически при True
+                "resize_keyboard" => false // можно заменить на FALSE, клавиатура будет использовать компактный размер автоматически при True
+            );
+            $param = '&reply_markup=' . json_encode($keyboard);
+
+        } elseif ($q == 1003) {
+            $message_t = 'Неверное значение поля xCode [' . $q . ']';
+        } else {
+            $message_t = 'Пришлите еще раз значение поля xCode.';
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+sendMessage($chat_id, $message_t, $param);
+/////////////////////////////////////
+exit();
 if($user_info) {
 
-    $user_info_row = json_decode($user_info);
+
 
     if($user_info_row->active == 0) {
         //аккаунт не активирован
@@ -141,142 +342,6 @@ if($user_info) {
         }
     } else {
 
-        if(empty($user_info_row->request)) {
-
-            $keyboard = array(
-                "keyboard" => array(
-                    array(
-                        array(
-                            "text" => "Создать жалобу"
-                        ),
-                        array(
-                            "text" => "Статус заявки"
-                        ),
-                        array(
-                            "text" => "Статистика за день"
-                        )
-                    )
-                ),
-                "one_time_keyboard" => false, // можно заменить на FALSE,клавиатура скроется после нажатия кнопки автоматически при True
-                "resize_keyboard" => false // можно заменить на FALSE, клавиатура будет использовать компактный размер автоматически при True
-            );
-            $param = '&reply_markup=' . json_encode($keyboard);
-
-            /******************************************************************************/
-            if($message == 'Создать жалобу') {
-                $message_t = $first_name . ", Введите 9-тизначный регистрационный номер заявки и текст сообщения, по которой у вас жалоба.\n\nПример, 71******* слишком молодая девушка";
-                $array = array(
-                    'action' => 'request_update',
-                    'type' => 'create_complaint',
-                    'chat_id' => $chat_id
-                );
-                $q = $query->xDriveQuery($array);
-            }
-            /******************************************************************************/
-            elseif ($message == 'Статус заявки') {
-                $message_t = $first_name . ", введите числовой номер заявки или регистрационный номер консультанта. ";
-                $array = array(
-                    'action' => 'request_update',
-                    'type' => 'application_status',
-                    'chat_id' => $chat_id
-                );
-                $q = $query->xDriveQuery($array);
-            }
-            /******************************************************************************/
-            elseif ($message == 'Статистика за день') {
-                $array = array(
-                    'action' => 'stat',
-                    'chat_id' => $chat_id
-                );
-                $q = $query->xDriveQuery($array);
-                $message_t = $q;
-            }
-            /******************************************************************************/
-            elseif($message == '/chat_id') {
-                $message_t = 'Ваш Chat_id: ' . $chat_id;
-            }
-            /******************************************************************************/
-            else {
-
-                $message_t = "Привет, " . $first_name . ".\n\nЕсли хотите пожаловаться на заявку - Нажмите на кнопку «Создать жалобу», если хотите проверить статус выполнения заявки - «Статус заявки» под клавиатурой.";
-            }
-            /******************************************************************************/
-        } else {
-
-
-            $keyboard = array(
-                "keyboard" => array(
-                    array(
-                        array(
-                            "text" => "Создать жалобу"
-                        ),
-                        array(
-                            "text" => "Статус заявки"
-                        ),
-                        array(
-                            "text" => "Статистика за день"
-                        )
-                    )
-                ),
-                "one_time_keyboard" => false, // можно заменить на FALSE,клавиатура скроется после нажатия кнопки автоматически при True
-                "resize_keyboard" => false // можно заменить на FALSE, клавиатура будет использовать компактный размер автоматически при True
-            );
-
-            $param = '&reply_markup=' . json_encode($keyboard);
-
-            //Если выполняется сложный запрос с вводом
-            $type = $user_info_row->request;
-            if($type == 'create_complaint') {
-                preg_match("/([0-9]*) (.*)/",$message,$m_arr);
-                if(!empty($m_arr[1]) && !empty($m_arr[2])) {
-
-                    if(strlen($m_arr[1]) == 9) {
-                        $array = array(
-                            'action'    => 'add',
-                            'number'    => $m_arr[1],
-                            'text'      => trim($m_arr[2]),
-                            'chat_id'   => $chat_id
-                        );
-                        $q = $query->xDriveQuery($array);
-
-
-                        if($q == 0) {
-                            $message_t = 'Ошибка добавления заявки. - ' . $message;
-                        } elseif($q == 2) {
-                            $message_t = 'В xDrive нет консультанта с регистрационным номером ' . $m_arr[1];
-                        } else {
-                            $message_t = 'Вашей заявке присвоен №' . $q . '. Мы отправим вам ответ в ближайшее время.';
-                        }
-                    } else {
-                        $message_t = "Ошибка!\n\nНеправильно введен регистрационный номер консультанта.\n\nПример, 71******* слишком молодая девушка";
-                    }
-
-
-                } else {
-                    $message_t = "Не правильно введен запрос.\n\nПример, 71******* слишком молодая девушка";
-                }
-            }
-            elseif($type == 'application_status') {
-                $id = preg_replace('![^0-9]+!', '', $message);
-                $array = array(
-                    'action'    => 'find',
-                    'id'        => $id,
-                    'chat_id'   => $chat_id
-                );
-                $q = $query->xDriveQuery($array);
-
-                if($q == 0) {
-                    $message_t = 'Заявка не найдена.' . $error_text;
-                } else {
-                    $message_t = $q;
-                }
-
-                $message_t = $q;
-            }
-            else {
-                $message_t = 'Ошибка выполнения запроса. Тип запроса не найден. ' . $error_text;
-            }
-        }
 
 
     }
@@ -286,6 +351,8 @@ if($user_info) {
 
 } else {
     //Формируем регистрацию
+    $my_request = '';
+
 
     /******************************************************************************/
     if(!empty($contact)) {
